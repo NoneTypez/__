cb7a4711bc95f65644b79fc54ee14e26231d6bf1
NoneTypez <graffity555@gmail.com>
2025-01-05 00:12:31 +0200

    piece of the structure

---
+++ src/components/button/button.tsx
@@ -0,0 +1,24 @@
+// function Button({
+//   svgIcon: SvgIconComponent,
+//   text = undefined,
+//   className,
+//   onClick = undefined,
+// }) {
+//   return (
+//     <button onClick={onClick}>
+//       {SvgIconComponent && <SvgIconComponent />}
+//       {text && (
+//         <span onClick={onClick} className={className}>
+//           {text}
+//         </span>
+//       )}
+//     </button>
+//   );
+// }
+
+// Button.propTypes = {
+//   svgIcon: PropTypes.elementType,
+//   text: PropTypes.string,
+//   className: PropTypes.string.isRequired,
+//   onClick: PropTypes.func.isRequired,
+// };
